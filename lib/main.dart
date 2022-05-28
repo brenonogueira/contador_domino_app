1d35a6294f22fd91d90fda980dc3366f0b5c4265
breno nogueira <breno.nog.araujo@hotmail.com>
2022-05-28 13:07:12 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:domino_app/screens/three_players.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ThreePlayers(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme:
+          ThemeData(brightness: Brightness.dark, primaryColor: Colors.blueGrey),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Escolha a quantidade de jogadores:',
+                style: TextStyle(fontSize: 20),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlinedButton.icon(
+                        onPressed: () {
+                          // Respond to button press
+                        },
+                        icon: Icon(Icons.add, size: 18),
+                        label: Text("3",  style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlinedButton.icon(
+                        onPressed: () {
+                          // Respond to button press
+                        },
+                        icon: Icon(Icons.add, size: 18),
+                        label: Text("4",  style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+     
+      bottomSheet: Container(
+        height: 50,
+        child: Center(
+          child: Text('Developed by: @brenonogr'),
+        ),
+      ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/three_players.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/material.dart';
+
+class ThreePlayers extends StatefulWidget {
+  const ThreePlayers({Key? key}) : super(key: key);
+
+  @override
+  State<ThreePlayers> createState() => _ThreePlayersState();
+}
+
+class _ThreePlayersState extends State<ThreePlayers> {
+  int _placar1 = 0;
+  int _placar2 = 0;
+
+  void _incrementPlacar1(int value) {
+    setState(() {
+      _placar1 += value;
+    });
+  }
+
+  void _incrementPlacar2(int value) {
+    setState(() {
+      _placar2 += value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme:
+          ThemeData(brightness: Brightness.dark, primaryColor: Colors.blueGrey),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dupla 1 x Dupla 2'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlinedButton(
+                        onPressed: () {
+                          _incrementPlacar1(-10);
+                        },
+                        child:
+                            Text('-10', style: TextStyle(color: Colors.black)),
+                        style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.red),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlinedButton(
+                        onPressed: () {
+                          _incrementPlacar1(5);
+                        },
+                        child: Text('5', style: TextStyle(color: Colors.black)),
+                        style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.blue),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlinedButton(
+                        onPressed: () {
+                          _incrementPlacar1(10);
+                        },
+                        child:
+                            Text('10', style: TextStyle(color: Colors.black)),
+                        style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.blue),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlinedButton(
+                        onPressed: () {
+                          _incrementPlacar1(50);
+                        },
+                        child: Text(
+                          '50',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.green),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlinedButton(
+                        onPressed: () {
+                          _incrementPlacar1(-50);
+                        },
+                        child:
+                            Text('-50', style: TextStyle(color: Colors.black)),
+                        style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.red),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Dupla 1",
+                            style: TextStyle(fontSize: 20, color: Colors.red)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "${_placar1}",
+                          style: TextStyle(fontSize: 50),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.only(top: 1),
+                    child: Divider(
+                      color: Colors.black,
+                    ),
+                  ))
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Dupla 2",
+                          style: TextStyle(fontSize: 20, color: Colors.blue)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "${_placar2}",
+                        style: TextStyle(fontSize: 50),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: OutlinedButton(
+                            onPressed: () {
+                              _incrementPlacar2(-10);
+                            },
+                            child: Text('-10',
+                                style: TextStyle(color: Colors.black)),
+                            style: OutlinedButton.styleFrom(
+                                backgroundColor: Colors.red),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: OutlinedButton(
+                            onPressed: () {
+                              _incrementPlacar2(5);
+                            },
+                            child: Text('5',
+                                style: TextStyle(color: Colors.black)),
+                            style: OutlinedButton.styleFrom(
+                                backgroundColor: Colors.blue),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: OutlinedButton(
+                            onPressed: () {
+                                _incrementPlacar2(10);
+                            },
+                            child: Text('10',
+                                style: TextStyle(color: Colors.black)),
+                            style: OutlinedButton.styleFrom(
+                                backgroundColor: Colors.blue),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: OutlinedButton(
+                            onPressed: () {
+                               _incrementPlacar2(50);
+                            },
+                            child: Text(
+                              '50',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                                backgroundColor: Colors.green),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: OutlinedButton(
+                            onPressed: () {},
+                            child: Text('-50',
+                                style: TextStyle(color: Colors.black)),
+                            style: OutlinedButton.styleFrom(
+                                backgroundColor: Colors.red),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
